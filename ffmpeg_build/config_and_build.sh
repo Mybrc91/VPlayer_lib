9f4cee74833ab22103e2b50080247aeedfd0168d
Matthew Ng <matthewn4444@gmail.com>
2014-07-17 23:30:28 -0400

    Added a config and build shell script to easily compile ffmpeg

---
+++ ffmpeg_build/config_and_build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Change location to the NDK8 folder
+if [ "$NDK" = "" ]; then
+    export NDK=~/Projects/ndk-8
+fi
+
+GIT_SSL_NO_VERIFY=true
+
+function addAutomakeOpts() {
+    if !(grep -Rq "AUTOMAKE_OPTIONS" Makefile.am)
+    then
+        sed -i '1iAUTOMAKE_OPTIONS=subdir-objects' Makefile.am
+    fi
+}
+
+git submodule update --init --recursive
+
+# Because it is svn...
+svn checkout http://libyuv.googlecode.com/svn/trunk/ libyuv
+
+# configure the environment
+cd freetype2
+sh ./autogen.sh
+cd ..
+
+# fribidi
+cd fribidi
+autoreconf -ivf
+cd ..
+
+# libass
+cd libass
+autoreconf -ivf
+cd ..
+
+# aacenc environment
+cd vo-aacenc
+addAutomakeOpts
+autoreconf -ivf
+cd ..
+
+# vo-amrwbenc environment
+cd vo-amrwbenc
+addAutomakeOpts
+autoreconf -ivf
+cd ..
+
+# Start the build!
+source build_android.sh
