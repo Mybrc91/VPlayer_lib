86e2142b8d35d08b78e9cfb34d95a9ca08b7f0dd
Matthew Ng <matthewn4444@gmail.com>
2014-07-19 18:13:55 -0400

    Changed listener to abstract instead of interface for less listener code

--- VPlayer_library/src/com/vplayer/VPlayerListener.java
+++ VPlayer_library/src/com/vplayer/VPlayerListener.java
@@ -3,11 +3,11 @@ package com.vplayer;
 import com.vplayer.exception.NotPlayingException;
 import com.vplayer.exception.VPlayerException;
 
-public interface VPlayerListener {
-    void onMediaSourceLoaded(VPlayerException err, MediaStreamInfo[] streams);
-    void onMediaResume(NotPlayingException result);
-    void onMediaPause(NotPlayingException err);
-    void onMediaStop();
-    void onMediaUpdateTime(long mCurrentTimeUs, long mVideoDurationUs, boolean isFinished);
-    void onMediaSeeked(NotPlayingException result);
+public abstract class VPlayerListener {
+    public void onMediaSourceLoaded(VPlayerException err, MediaStreamInfo[] streams) {}
+    public void onMediaResume(NotPlayingException result) {}
+    public void onMediaPause(NotPlayingException err) {}
+    public void onMediaStop() {}
+    public void onMediaUpdateTime(long mCurrentTimeUs, long mVideoDurationUs, boolean isFinished) {}
+    public void onMediaSeeked(NotPlayingException result) {}
 }
