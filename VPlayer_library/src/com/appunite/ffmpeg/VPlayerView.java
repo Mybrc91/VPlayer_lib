a7006237bd50c33467f524e37169fddb18a29efd
Matthew Ng <matthewn4444@gmail.com>
2014-07-18 21:07:25 -0400

    Created one view that handles the player and the surface view for easier integration

---
+++ VPlayer_library/src/com/appunite/ffmpeg/VPlayerView.java
@@ -0,0 +1,81 @@
+package com.appunite.ffmpeg;
+
+import android.app.Activity;
+import android.util.AttributeSet;
+import android.view.WindowManager;
+
+public class VPlayerView extends FFmpegSurfaceView {
+    private final FFmpegPlayer mPlayer;
+    private final Activity mAct;
+    private boolean mIsPlaying;
+
+    public VPlayerView(Activity activity) {
+        this(activity, null);
+    }
+
+    public VPlayerView(Activity activity, AttributeSet attrs) {
+        this(activity, attrs, 0);
+    }
+
+    public VPlayerView(Activity activity, AttributeSet attrs, int defStyle) {
+        super(activity, attrs, defStyle);
+        mAct = activity;
+        mPlayer = new FFmpegPlayer(this, activity);
+        mIsPlaying = false;
+    }
+
+    public void setWindowFullscreen() {
+        mAct.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mAct.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        mAct.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    public void clearWindowFullscreen() {
+        mAct.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mAct.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    public void setVideoPath(String path) {
+        mPlayer.setDataSource(path);
+    }
+
+    public void setVideoListener(FFmpegListener listener) {
+        mPlayer.setMpegListener(listener);
+    }
+
+    public void onPause() {
+        if (mIsPlaying) {
+            pause();
+        }
+    }
+
+    public void onResume() {
+        if (!mIsPlaying) {
+            play();
+        }
+    }
+
+    public void onDestroy() {
+        mPlayer.stop();
+    }
+
+    public void pause() {
+        mIsPlaying = false;
+        mPlayer.pause();
+    }
+
+    public void seek(long positionUs) {
+        mPlayer.seek(positionUs);
+    }
+
+    public void play() {
+        mIsPlaying = true;
+        mPlayer.resume();
+    }
+
+    public void stop() {
+        pause();
+        mPlayer.seek(0);
+    }
+
+}
