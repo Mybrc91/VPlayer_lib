d5c02a5af067b13848f2a754dc3e053a4f26cf6e
Matthew Ng <matthewn4444@gmail.com>
2015-01-01 12:04:56 -0500

    Removed all from Application. mk because we cannot compile 64bit yet

--- VPlayer_library/jni/Application.mk
+++ VPlayer_library/jni/Application.mk
@@ -13,13 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# The ARMv7 is significanly faster due to the use of the hardware FPU
-APP_ABI := all
-#APP_ABI := armeabi-v7a  armeabi x86
-#APP_ABI  := x86
-#APP_ABI  := mips
-#APP_ABI := armeabi-v7a
+APP_ABI := armeabi-v7a armeabi x86 mips
 APP_PLATFORM := android-10
-#APP_OPTIM := debug
+APP_OPTIM := release
 
 APP_STL := gnustl_static
